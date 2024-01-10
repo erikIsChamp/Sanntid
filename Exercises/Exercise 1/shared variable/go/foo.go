a7bcb9606ccd178f8e302262b05753f0f88f460e
Erik <erikgrieg@hotmail.com>
2024-01-10 13:05:12 +0100

    progress

--- Exercises/Exercise 1/shared variable/go/foo.go
+++ Exercises/Exercise 1/shared variable/go/foo.go
@@ -3,29 +3,49 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
-    "time"
+	"fmt"
+	. "fmt"
+	"runtime"
+	"time"
 )
 
 var i = 0
 
 func incrementing() {
-    //TODO: increment i 1000000 times
+	// TODO: increment i 1000000 times
+	for j := 0; j <= 1000000; j++ {
+		i++
+	}
 }
 
 func decrementing() {
-    //TODO: decrement i 1000000 times
+	//TODO: decrement i 1000000 times
+	for j := 0; j <= 1000000; j++ {
+		i--
+	}
 }
 
 func main() {
-    // What does GOMAXPROCS do? What happens if you set it to 1?
-    runtime.GOMAXPROCS(2)    
-	
-    // TODO: Spawn both functions as goroutines
-	
-    // We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
-    // We will do it properly with channels soon. For now: Sleep.
-    time.Sleep(500*time.Millisecond)
-    Println("The magic number is:", i)
+	// What does GOMAXPROCS do? What happens if you set it to 1?
+	// It enables the use of 2 threads to be used by Go. If you set it to 1, only 1 thread can be used
+	runtime.GOMAXPROCS(2)
+
+	// TODO: Spawn both functions as goroutines
+
+	// Start the first goroutine
+	go func() {
+		incrementing()
+	}()
+
+	// Start the second goroutine
+	go func() {
+		decrementing()
+	}()
+
+	fmt.Println("Both goroutines have completed.")
+
+	// We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
+	// We will do it properly with channels soon. For now: Sleep.
+	time.Sleep(500 * time.Millisecond)
+	Println("The magic number is:", i)
 }
