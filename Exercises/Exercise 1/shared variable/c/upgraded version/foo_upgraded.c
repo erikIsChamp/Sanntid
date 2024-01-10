aada4ee37461e98cb8b9328c424d218c972443fc
Erik <erikgrieg@hotmail.com>
2024-01-10 15:05:35 +0100

    oppg.4 start

--- Exercises/Exercise 1/shared variable/c/upgraded version/foo_upgraded.c
+++ Exercises/Exercise 1/shared variable/c/upgraded version/foo_upgraded.c
@@ -3,13 +3,22 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <semaphore.h>
 
 // global shared int i = 0
 volatile int i = 0;
 
 
+// Declare a mutex
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
 // Note the return type: void*
-void* incrementingThreadFunction(){
+void* incrementingThreadFunction(void *arg){
+
+    // Lock the mutex to gain exclusive access to the shared resource
+    pthread_mutex_lock(&mutex);
+
     // TODO: increment i 1_000_000 times
     for (int j = 0; j <= 1000000; j++){
         i++;
@@ -17,9 +26,12 @@ void* incrementingThreadFunction(){
     return NULL;
 }
 
-void* decrementingThreadFunction(){
+void* decrementingThreadFunction(void *arg){
+    // Wait for the semaphore
+    sem_wait(&semaphore);
+
     // TODO: decrement i 1_000_000 times
-    for (int j = 0; j <= 1000000; j++){
+    for (int j = 0; j <= 999999; j++){
         i--;
     }
     return NULL;
@@ -32,6 +44,12 @@ int main(){
     // Hint: search the web! Maybe try "pthread_create example"?
 
     pthread_t thread1, thread2; // Variables to hold thread identifiers
+
+    // Initialize the semaphore with an initial value of 1
+    if (sem_init(&semaphore, 0, 1) != 0) {
+        perror("sem_init");
+        return 1;
+    }
 
     // Create the first thread
     if (pthread_create(&thread1, NULL, incrementingThreadFunction, NULL) != 0) {
@@ -59,6 +77,9 @@ int main(){
         perror("pthread_join");
         return 1;
     }
+
+    // Destroy the semaphore
+    sem_destroy(&semaphore);
 
     printf("Both threads have finished\n");
     
--- Exercises/Exercise 1/shared variable/result.md
+++ Exercises/Exercise 1/shared variable/result.md
@@ -1,3 +1,5 @@
 ## Threads in C and Go
 
 The magic number is not always zero since the 2 threads are simultaneously working on with the same variable. This causes the number to change every time we run the code.
+
+When setting the runtime.GOMAXPROCS(1) to 1, only 1 thread can run at a time. The functions will run after one another.
