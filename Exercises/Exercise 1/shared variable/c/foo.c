faf9aab3d0b153098c17b45032443cc9ff0bda6f
Lars Hammernes Leopold <lars.leopold@hotmail.no>
2024-01-10 13:19:07 +0100

    copypasta erik sin kode

--- Exercises/Exercise 1/shared variable/c/foo.c
+++ Exercises/Exercise 1/shared variable/c/foo.c
@@ -4,17 +4,24 @@
 #include <pthread.h>
 #include <stdio.h>
 
-int i = 0;
+// global shared int i = 0
+volatile int i = 0;
+
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
-    noe
+    for (int j = 0; j <= 1000000; j++){
+        i++;
+    }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+    for (int j = 0; j <= 1000000; j++){
+        i--;
+    }
     return NULL;
 }
 
@@ -23,10 +30,38 @@ int main(){
     // TODO: 
     // start the two functions as their own threads using `pthread_create`
     // Hint: search the web! Maybe try "pthread_create example"?
-    
+
+    pthread_t thread1, thread2; // Variables to hold thread identifiers
+
+    // Create the first thread
+    if (pthread_create(&thread1, NULL, incrementingThreadFunction, NULL) != 0) {
+        perror("pthread_create");
+        return 1;
+    }
+
+    // Create the second thread
+    if (pthread_create(&thread2, NULL, decrementingThreadFunction, NULL) != 0) {
+        perror("pthread_create");
+        return 1;
+    }
+
     // TODO:
     // wait for the two threads to be done before printing the final result
-    // Hint: Use `pthread_join`    
+    // Hint: Use `pthread_join`   
+
+        // Wait for both threads to finish
+    if (pthread_join(thread1, NULL) != 0) {
+        perror("pthread_join");
+        return 1;
+    }
+
+    if (pthread_join(thread2, NULL) != 0) {
+        perror("pthread_join");
+        return 1;
+    }
+
+    printf("Both threads have finished\n");
+    
     
     printf("The magic number is: %d\n", i);
     return 0;
