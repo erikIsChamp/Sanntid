48299d7ea3cd96eef3e72e0a58c7147a664f8e18
Lars Hammernes Leopold <lars.leopold@hotmail.no>
2024-01-12 17:40:04 +0100

    synced with channels and select

--- Exercises/Exercise 1/shared variable/go/foo.go
+++ Exercises/Exercise 1/shared variable/go/foo.go
@@ -6,43 +6,55 @@ import (
     . "fmt"
     "runtime"
     "time"
-    "sync"
+    
 )
 
-var (
-    i = 0
-    lock sync.Mutex
+var i int = 0
 
-)
-
-func incrementing() {
+func incrementing(inc chan int, c chan bool) {
     //TODO: increment i 1000000 times
     for j := 0; j < 1000000; j++ {
-        lock.Lock()
+        
         i++
-        lock.Unlock()
+        inc <- i
+    
     }
+    c <- true
 }
 
-func decrementing() {
+func decrementing(dec chan int, c chan bool) {
     //TODO: decrement i 1000000 times
     for j := 0; j < 1000001; j++ {
-        lock.Lock()
         i--
-        lock.Unlock()
+        dec <- i
     }
+    c <- true
 }
 
 func main() {
     // What does GOMAXPROCS do? What happens if you set it to 1?
-    runtime.GOMAXPROCS(2)    
+    runtime.GOMAXPROCS(2)
+    inc := make(chan int,1)
+    dec := make(chan int,1)
+    done := make(chan bool,2)
 	
     // TODO: Spawn both functions as goroutines
-    go incrementing()
-    go decrementing()
+    go incrementing(inc,done)
+    go decrementing(dec,done)
+    
+    for j := 0; j < 1; j++ {
+        select {   
+        case <- inc:
+        case <- dec:
+        case <- done:
+    
+        }
+    }
 	
     // We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
     // We will do it properly with channels soon. For now: Sleep.
     time.Sleep(500*time.Millisecond)
     Println("The magic number is:", i)
 }
+
+
